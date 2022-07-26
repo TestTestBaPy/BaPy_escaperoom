b0773d917d1f3edaa836878ac7948d1422deaf2a
Jasmin Bödiker <jboediker@uni-osnabrueck.de>
2022-07-26 16:12:09 +0200

    Musik

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from game_timer import *
 pygame.init()
 
 # First of all we want some nice backgroundmusic, therefore we load some mp3, we want that the music repeats the whole time, so we set -1 and we set our volume to 0.1
-pygame.mixer.music.load('Sounds/LawAndOrder.mp3')
+pygame.mixer.music.load('Sounds/ALeagueOfTheirOwn.mp3')
 pygame.mixer.music.play(-1,0.0)
 pygame.mixer.music.set_volume(.1)
 
