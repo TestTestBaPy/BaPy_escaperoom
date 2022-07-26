ad8afbae1e47119ee7d3aa31801ba2e90a81b7e3
Lara0000 <hamstergeorge01@gmail.com>
2022-07-26 15:29:13 +0200

    bird

--- display_components.py
+++ display_components.py
@@ -53,10 +53,9 @@ correct = pygame.mixer.Sound('Sounds/correct-6033.wav')
 cloth_sound = pygame.mixer.Sound('Sounds/Tuch-schieben.wav')
 rclick = pygame.mixer.Sound('Sounds/rclick.mp3')
 page = pygame.mixer.Sound('Sounds/page.mp3')
-
 soundObj = pygame.mixer.Sound('Sounds/Meeresrauschen.wav')
 Button = pygame.mixer.Sound('Sounds/Button.mp3')
-
+bird = pygame.mixer.Sound('Sounds/bird.mp3')
 
 def text_objects(text, font):
     textSurface = font.render(text, True, (0, 0, 0))
